c1a5b7bf293fbab46431e6a87fbb4f5ee025a93e
Pavel <pmk24031997@gmail.com>
2022-02-16 22:00:22 +0300

    2

--- Lesson_1/Ex_4.py
+++
@@ -1,9 +0,0 @@
-a = int(input("Введите число: "))
-n = 0
-m = 0
-while a // 10 > 0:
-    m = a % 10
-    if m > n:
-        n = m
-    a = a // 10
-print(n)
--- Lesson_1/Ex_5.py
+++
@@ -1,8 +0,0 @@
-rev = int(input('Введите объем выручки: '))
-costs= int(input('Введите объем издержек: '))
-if rev > costs:
-    print('Прибыль')
-elif rev== costs:
-    print('Так себе поработли')
-else:
-    print('Убыток')
