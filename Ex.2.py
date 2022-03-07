5f8eabba717670edb29a40c172a205ecf3321635
Pavel <pmk24031997@gmail.com>
2022-03-07 23:52:23 +0300

    New structure2

---
+++ Ex.2.py
@@ -0,0 +1,9 @@
+def count_info():
+    try:
+        with open('test.txt', 'r', encoding="utf-8") as file:
+            my_list = file.readlines()
+            for i in range(len(my_list)):
+                new_l = my_list[i].split()
+                print(f'Количество строк в файле {len(my_list)}. В {i + 1}-ой строке {len(new_l)} слов(а)')
+    except FileNotFoundError:
+        return 'Файл не найден.'
